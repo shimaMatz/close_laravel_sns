32949a7521e57f1a716720af151eca6ae4a8c371
shimamatz@gmail.com <shimamatz@gmail.com>
2021-09-24 04:18:52 +0900

    いいねを非同期通信を用いてDB保存をする

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Article;
-
 use App\Http\Requests\ArticleRequest;
-
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -18,6 +16,7 @@ class ArticleController extends Controller
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
+
         return view('articles.index', ['articles' => $articles]);
     }
 
@@ -31,28 +30,52 @@ class ArticleController extends Controller
         $article->fill($request->all());
         $article->user_id = $request->user()->id;
         $article->save();
+
         return redirect()->route('articles.index');
     }
 
     public function edit(Article $article)
     {
-        return view('articles.edit',['article' => $article]);
+        return view('articles.edit', ['article' => $article]);
     }
 
     public function update(ArticleRequest $request, Article $article)
     {
         $article->fill($request->all())->save();
+
         return redirect()->route('articles.index');
     }
 
     public function destroy(Article $article)
     {
         $article->delete();
+
         return redirect()->route('articles.index');
     }
 
     public function show(Article $article)
     {
         return view('articles.show', ['article' => $article]);
+    }
+
+    public function like(Request $request, Article $article)
+    {
+        $article->likes()->detach($request->user()->id);
+        $article->likes()->attach($request->user()->id);
+
+        return [
+            'id' => $article->id,
+            'countLikes' => $article->count_likes,
+        ];
+    }
+
+    public function unlike(Request $request, Article $article)
+    {
+        $article->likes()->detach($request->user()->id);
+
+        return [
+            'id' => $article->id,
+            'countLikes' => $article->count_likes,
+        ];
     }
 }
