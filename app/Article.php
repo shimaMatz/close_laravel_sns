b1340f05e75f1e1bc10119e54c2f36a672c731ae
shimamatz@gmail.com <shimamatz@gmail.com>
2021-09-24 00:54:05 +0900

    ユーザーがいいね済みか判定メソッド追加

--- app/Article.php
+++ app/Article.php
@@ -12,9 +12,20 @@ class Article extends Model
         'body',
     ];
 
-
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
+    }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    public function isLikedby(?User $user): bool
+    {
+        return $user
+        ? (bool) $this->likes->where('id', $user->id)->count()
+        : false;
     }
 }
